1c89b22ad5e3d03f8db556e7811fd5c9020a1171
Mukhammad Restu <mrestu696@gmail.com>
2017-01-28 17:05:34 +0700

    Laravel

---
+++ detail.blade.php
@@ -0,0 +1,20 @@
+<h3> Ini Halaman Produk </h3>
+
+<p> Nama Produk yang Anda Pakai adalah </p>
+
+
+
+{{$merk}}
+
+<br>
+
+<?php
+
+if ($merk == "odol"){
+	echo "Kenapa Harus Odol ?";
+	
+}else{
+	echo "panjang";
+}
+ 
+?>
